d577f049e416786709f3d4b3efd1dbe7e22b080f
d-jagoda <>
2023-01-23 23:05:10 +0000

    Script for configuring network when docker starts

---
+++ add-wsl-routes.bat
@@ -0,0 +1,4 @@
+cd "%~dp0"
+%~d0
+
+wsl.exe -d docker-desktop -u root ./add-wsl-routes.sh
---
+++ win-auto-start.ps1
@@ -0,0 +1,24 @@
+
+Set-Location $PSScriptRoot 
+
+while ($true) {
+    $pipeName = "docker_engine_linux"
+    $dockerPipe = new-object System.IO.Pipes.NamedPipeClientStream '.', $pipeName , 'IN'
+    try {
+        Write-Host "[$(Get-Date)] Attempting to connect to $pipeName"
+        $dockerPipe.Connect()
+        Write-Host "[$(Get-Date)] Connect to $pipeName. Docker engine is running"
+
+        wsl.exe -d docker-desktop -u root ./add-wsl-routes.sh
+
+        [byte[]] $buffer = @(0) * 1024
+        $readCount = 0;
+        do {
+            $readCount = $dockerPipe.Read($buffer, 0, $buffe.Length)
+        } while ($readCount > 0)
+
+        Write-Host "[$(Get-Date)] Disconnected from $pipeName. Assuming Docker engine was shutdown"
+    } finally {
+        $dockerPipe.Dispose()
+    }
+}
